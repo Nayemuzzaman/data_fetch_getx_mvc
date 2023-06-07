7373b26b1f9b566112621521486a655fcd6bca3b
nayemuzzaman <nayemzaman@gmail.com>
2023-06-08 01:45:26 +0900

    first step

---
+++ android/app/src/main/kotlin/com/example/data_fetch_getx_mvc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.data_fetch_getx_mvc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,2 @@
+// Step 1
+const BASE_URL = "https://fakestoreapi.com/products/category/jewelery";
